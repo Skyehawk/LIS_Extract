3aeaf996ec247f6ad4c582ae1198ac21bc7a71df
skye.skyehawk@gmail.com <dnaKgGyuWKi4>
2020-03-28 18:13:43 -0400

    attempt to plot with rest of output

--- NEXRADL3_Extract.py
+++ NEXRADL3_Extract.py
@@ -35,6 +35,7 @@ from Gauss_Map import gauss_map
 #  --- Construct argument parse to parse the arguments (input dates) ---
 ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--NEXRADL3", help=" path to the input file of NEXRADL3 Data ()")
+ap.add_argument("-l", "--LIS", help=" path to the input file of LIS data (GRIB)")
 ap.add_argument("-g", "--GFS", help=" path to the input file of GFS Wind U*V data (GRIB)")
 ap.add_argument("-o", "--output", help=" path to the output directory")
 ap.add_argument("-c", "--lat_lon", nargs="+", help="passed: -c <lat_float> <lon_float>; Lat/Lon of point of convection")
@@ -42,27 +43,27 @@ args = vars(ap.parse_args())
 outputPath = args["output"]
 
 # --- test ---
-fig, axes = plt.subplots(1, 1, figsize=(15, 8))
-for v, ctable, ax in zip(('N0Q', 'N0U'), ('NWSReflectivity', 'NWSVelocity'), axes):
-	# Open the file
-	f = Level3File(args["NEXRADL3"])
+#fig, axes = plt.subplots(1, 1, figsize=(15, 8))
+#for v, ctable, ax in zip(('N0Q', 'N0U'), ('NWSReflectivity', 'NWSVelocity'), axes):
+#	# Open the file
+#	f = Level3File(args["NEXRADL3"])
 
 	# Pull the data out of the file object
-	datadict = f.sym_block[0][0]
+#	datadict = f.sym_block[0][0]
 
 	# Turn into an array, then mask
-	data = np.ma.array(datadict['data'])
-	data[data == 0] = np.ma.masked
+#	data = np.ma.array(datadict['data'])
+#	data[data == 0] = np.ma.masked
 
 	# Grab azimuths and calculate a range based on number of gates
-	az = np.array(datadict['start_az'] + [datadict['end_az'][-1]])
-	rng = np.linspace(0, f.max_range, data.shape[-1] + 1)
+#	az = np.array(datadict['start_az'] + [datadict['end_az'][-1]])
+#	rng = np.linspace(0, f.max_range, data.shape[-1] + 1)
 
 	# Convert az,range to x,y
-	xlocs = rng[:-1] * np.sin(np.deg2rad(az[1:, None]))/111.0 + 2.131
-	ylocs = rng[:-1] * np.cos(np.deg2rad(az[1:, None]))/111.0 + 0.456
-	print(np.shape(xlocs))
-	print(np.shape(data))
+#	xlocs = rng[:-1] * np.sin(np.deg2rad(az[1:, None]))/111.0 + 2.131
+#	ylocs = rng[:-1] * np.cos(np.deg2rad(az[1:, None]))/111.0 + 0.456
+#	print(np.shape(xlocs))
+#	print(np.shape(data))
 	#print(rGriddedVals[:10,:10])
 	
 
@@ -71,57 +72,55 @@ for v, ctable, ax in zip(('N0Q', 'N0U'), ('NWSReflectivity', 'NWSVelocity'), axe
 
 
 	# --- Calculate coordinates bounding init point based on surface vector(s) (radians) ---
-	baseCrds = np.array([(1.0,0.5,0.0,1.0),(1.0,-0.5,0.0,1.0),(-0.125,-0.5,0.0,1.0),(-0.125,0.5,0.0,1.0),(1.0,0.5,0.0,1.0)]) 	#crds of bounding box (Gridded degrees)
-	testLocBearing = 0
-	testLoc = np.array([0,0])
+#	baseCrdsRadar = np.array([(1.0,0.5,0.0,1.0),(1.0,-0.5,0.0,1.0),(-0.125,-0.5,0.0,1.0),(-0.125,0.5,0.0,1.0),(1.0,0.5,0.0,1.0)]) 	#crds of bounding box (Gridded degrees)
+#	testLocRadarBearing = 0
+#	testLocRadar = np.array([0,0])
 	#comp_matrix(scale, rotation, shear, translation)
-	TM = comp_matrix(np.ones(3), np.array([0,0, testLocBearing]), np.ones(3), np.pad(testLoc, (0, 1), 'constant'))
-	polyVerts = TM.dot(baseCrds.T).T[:,:2]										#apply transformation Matrix, remove padding, and re-transpose
-	print("PolyVerts (Lon_Lat): ", polyVerts)
+#	TMRadar = comp_matrix(np.ones(3), np.array([0,0, testLocRadarBearing]), np.ones(3), np.pad(testLoc, (0, 1), 'constant'))
+#	polyVertsRadar = TMRadar.dot(baseCrdsRadar.T).T[:,:2]										#apply transformation Matrix, remove padding, and re-transpose
+#	print("PolyVertsRadar (Lon_Lat): ", polyVertsRadar)
 
 	# --- Generate ROI from coordiantes (above) create 2D boolean array to mask with ---
-	xp,yp = xlocs.flatten(),ylocs.flatten()
-	points = np.vstack((xp,yp)).T
-	path = Path(polyVerts)
-	grid = path.contains_points(points)
-	grid = grid.reshape(np.shape(xlocs))
-	rDataMasked = np.ma.masked_array(data, np.invert(grid))
+#	xpRadar,ypRadar = xlocs.flatten(),ylocs.flatten()
+#	pointsRadar = np.vstack((xp,yp)).T
+#	pathRadar = Path(polyVertsRadar)
+#	gridRadar = pathRadar.contains_points(pointsRadar)
+#	gridRadar = gridRadar.reshape(np.shape(xlocs))
+#	rDataMasked = np.ma.masked_array(data, np.invert(gridRadar))
 
 	# --- Clip our masked array, create sub-array of data and rotate ---
-	i, j = np.where(grid)
-	indices = np.meshgrid(np.arange(min(i), max(i) + 1), np.arange(min(j), max(j) + 1), indexing='ij')
-	rDataMaskedClip = data[indices]
-	rDataMaskClip = grid[indices]
-	print("dimsPreTM", rDataMaskedClip.shape)
-	print("maskDimsPreTM", rDataMaskClip.shape)
-	rDataMaskedClip = rDataMaskedClip*rDataMaskClip
-	print("dimsPostTM", rDataMaskedClip.shape)
+#	iRadar, jRadar = np.where(gridRadar)
+#	indicesRadar = np.meshgrid(np.arange(min(iRadar), max(iRadar) + 1), np.arange(min(jRadar), max(jRadar) + 1), indexing='ij')
+#	rDataMaskedClip = data[indicesRadar]
+#	rDataMaskClip = gridRadar[indicesRadar]
+#	print("dimsPreTM", rDataMaskedClip.shape)
+#	print("maskDimsPreTM", rDataMaskClip.shape)
+#	rDataMaskedClip = rDataMaskedClip*rDataMaskClip
+#	print("dimsPostTM", rDataMaskedClip.shape)
 
 # Plot the data
-	deltaX = 0#-236.541
-	deltaY = 0#-50.616
-	negXLim = -1#(-30 + deltaX )
-	posXLim = 1#(111 + deltaX )
-	negYLim = -1#(-111 + deltaY )
-	posYLim = 1#(111 + deltaY )
-	norm, cmap = colortables.get_with_steps(ctable, 16, 16)
+#--
+#	deltaX = 0#-236.541
+#	deltaY = 0#-50.616
+#	negXLim = -1#(-30 + deltaX )
+#	posXLim = 1#(111 + deltaX )
+#	negYLim = -1#(-111 + deltaY )
+#	posYLim = 1#(111 + deltaY )
+#	norm, cmap = colortables.get_with_steps(ctable, 16, 16)
 
-	ax.pcolormesh(xlocs[indices], ylocs[indices], rDataMaskedClip, norm=norm, cmap=cmap)
+#	ax.pcolormesh(xlocs[indices], ylocs[indices], rDataMaskedClip, norm=norm, cmap=cmap)
 	#ax.pcolormesh(xlocs, ylocs, data, norm=norm, cmap=cmap)
-	ax.set_aspect('equal', 'datalim')
-	ax.set_xlim(negXLim, posXLim)
-	ax.set_ylim(negYLim, posYLim)
-	add_timestamp(ax, f.metadata['prod_time'], y=0.02, high_contrast=True)
+#	ax.set_aspect('equal', 'datalim')
+#	ax.set_xlim(negXLim, posXLim)
+#	ax.set_ylim(negYLim, posYLim)
+#	add_timestamp(ax, f.metadata['prod_time'], y=0.02, high_contrast=True)
 
 #plt.show()
-plt.savefig(args["output"] +'_Test_NRL3_.png') # Set the output file name
+#plt.savefig(args["output"] +'_Test_NRL3_.png') # Set the output file name
 
 
 
-
-
-
-# --- Read in 3 datasets: 1) LIS Data for layer of intrest 2) GFS U (Easting) component of wind velocity 3) GFS V (Northing) component of wind velocity ---
+# --- Read in 4 datasets: 1) LIS Data for layer of intrest 2) GFS U (Easting) component of wind velocity 3) GFS V (Northing) component of wind velocity 4)NEXRAD L3 base reflectivity (long)---
 LISGrbs = pygrib.open(args["LIS"])
 LISGrb = LISGrbs.select()[32]												# index positions of relevent data
 print("\nLIS Data: " + str(LISGrb))
@@ -134,6 +133,12 @@ U = UGrb.values * 1.944														# array containing U component gridded valu
 VGrb = GFSGrbs.select(name='10 metre V wind component')[0]
 print("\nV Data: " + str(VGrb))
 V = VGrb.values * 1.944														# array containing V component gridded values (knots)
+
+rFile = Level3File(args["NEXRADL3"])
+rDataDict = rFile.sym_block[0][0]											# Pull the radar data out of the file object
+rData = np.ma.array(rDataDict['data'])										# Turn into an array, then mask
+rData[rData == 0] = np.ma.masked					
+
 
 # --- Convert gridded data to lat/lon information ---
 LISLons = np.linspace(float(LISGrb['longitudeOfFirstGridPointInDegrees']), float(LISGrb['longitudeOfLastGridPointInDegrees']), int(LISGrb['Ni']) )	#generate x coordinates normalized to the range of data
@@ -150,58 +155,89 @@ VLats = np.linspace(float(VGrb['latitudeOfFirstGridPointInDegrees']), float(VGrb
 V, VLons = shiftgrid(180., V, VLons, start=False)  							# use this to bump data from 0..360 on the lons to -180..180
 VGridLon, VGridLat = np.meshgrid(VLons, VLats) 								# regularly spaced 2D grid of GFS V component values
 
+# --- Grab azimuths and calculate a range based on number of gates ---
+rAz = np.array(rDataDict['start_az'] + [rDataDict['end_az'][-1]])
+rRng = np.linspace(0, rFile.max_range, rData.shape[-1] + 1)
+xlocs = rRng[:-1] * np.sin(np.deg2rad(rAz[1:, None]))/111.0 + 2.131 
+ylocs = rRng[:-1] * np.cos(np.deg2rad(rAz[1:, None]))/111.0 + 0.456 
+
 # get wind vector from the nearest grid cell
 #testLoc = np.array([-102.210999, 43.824593])								#2017/06/11
 #testLoc = np.array([-99.184, 41.217])										#2017/06/14
 #testLoc = np.array([-98.066, 45.456])										#2017/07/25
 
-testLoc = np.array([float(args["lat_lon"][1]),float(args["lat_lon"][0])])
-gridTestLoc = np.around(testLoc*2)/2										# round to the nearest 1.0 for GFS3, 0.5 for GFS4
+testLocROI = np.array([float(args["lat_lon"][1]),float(args["lat_lon"][0])])
+gridTestLoc = np.around(testLocROI*2)/2										# round to the nearest 1.0 for GFS3, 0.5 for GFS4
 testLocIdx= np.where(gridTestLoc[1]==UGridLat)[0][0], np.where(gridTestLoc[0]==UGridLon)[1][0]
 testValU = U[testLocIdx[0]-1: testLocIdx[0] + 2,testLocIdx[1]-1: testLocIdx[1] + 2]			# 3x3 sample of U values centered about our closest vector
 testValV = V[testLocIdx[0]-1: testLocIdx[0] + 2,testLocIdx[1]-1: testLocIdx[1] + 2]			# 3x3 sample of V values centered about our closest vector
 gausKern3x3sig1 = np.array([[0.077847,0.123317,0.077847],\
 						[0.123317,0.195346,0.123317],\
 						[0.077847,0.123317,0.077847]])
-testLocBearing = np.arctan2(np.sum(testValV*gausKern3x3sig1), np.sum(testValU*gausKern3x3sig1))
-testLocMag = np.sqrt(np.sum(testValV*gausKern3x3sig1)**2 + np.sum(testValU*gausKern3x3sig1)**2)
+testLocROIBearing = np.arctan2(np.sum(testValV*gausKern3x3sig1), np.sum(testValU*gausKern3x3sig1))
+testLocROIMag = np.sqrt(np.sum(testValV*gausKern3x3sig1)**2 + np.sum(testValU*gausKern3x3sig1)**2)
 
-print("input Loc: ", testLoc)
+print("input Loc: ", testLocROI)
 print("Nearest GFS Vector Loc: ", gridTestLoc)
-print("Nearest Vector --> Bearing: %3.2f from East (CCW) @ %3.1f knots (3x3 kernal gausian avg (sigma=1.0))" % (testLocBearing* 180 / np.pi, testLocMag))
+print("Nearest Vector --> Bearing: %3.2f from East (CCW) @ %3.1f knots (3x3 kernal gausian avg (sigma=1.0))" % (testLocROIBearing* 180 / np.pi, testLocROIMag))
 print("Variance of wind components --> U: %3.2f  V: %3.2f" % (np.var(U),np.var(V)))
 print("Variance of wind direction --> %3.2f " % (np.var(np.arctan2(testValV,testValU)*180/np.pi)))
 
-# --- Calculate coordinates bounding init point based on surface vector(s) (radians) ---
-baseCrds = np.array([(1.0,1.0,0.0,1.0),(1.0,-1.0,0.0,1.0),(-1.0,-1.0,0.0,1.0),(-1.0,1.0,0.0,1.0),(1.0,1.0,0.0,1.0)]) 	#crds of bounding box (Gridded degrees)
+testLocRadar = np.array([0,0])
+testLocRadarBearing = 0
+testLocRadarMag = 0
 
-#comp_matrix(scale, rotation, shear, translation)
-TM = comp_matrix(np.ones(3), np.array([0,0, testLocBearing]), np.ones(3), np.pad(testLoc, (0, 1), 'constant'))
-polyVerts = TM.dot(baseCrds.T).T[:,:2]										#apply transformation Matrix, remove padding, and re-transpose
-print("PolyVerts (Lon_Lat): ", polyVerts)
+# --- Calculate coordinates bounding init point based on surface vector(s) (radians) ---
+baseCrdsROI = np.array([(1.0,1.0,0.0,1.0),(1.0,-1.0,0.0,1.0),(-1.0,-1.0,0.0,1.0),(-1.0,1.0,0.0,1.0),(1.0,1.0,0.0,1.0)]) 	#crds of bounding box (Gridded degrees)
+#baseCrdsRadar = np.array([(1.0,0.5,0.0,1.0),(1.0,-0.5,0.0,1.0),(-0.125,-0.5,0.0,1.0),(-0.125,0.5,0.0,1.0),(1.0,0.5,0.0,1.0)]) 	#crds of bounding box (Gridded degrees)
+baseCrdsRadar = np.array([(1.0,1.0,0.0,1.0),(1.0,-1.0,0.0,1.0),(-1.0,-1.0,0.0,1.0),(-1.0,1.0,0.0,1.0),(1.0,1.0,0.0,1.0)])
+	# comp_matrix(scale, rotation, shear, translation)
+TMROI = comp_matrix(np.ones(3), np.array([0,0, testLocROIBearing]), np.ones(3), np.pad(testLocROI, (0, 1), 'constant'))
+polyVertsROI = TMROI.dot(baseCrdsROI.T).T[:,:2]										# apply transformation Matrix, remove padding, and re-transpose
+print("PolyVertsROI (Lon_Lat): ", polyVertsROI)
+
+TMRadar = comp_matrix(np.ones(3), np.array([0,0, testLocRadarBearing]), np.ones(3), np.pad(testLocRadar, (0, 1), 'constant'))
+polyVertsRadar = TMRadar.dot(baseCrdsRadar.T).T[:,:2]								# apply transformation Matrix, remove padding, and re-transpose
+print("PolyVertsRadar (Lon_Lat): ", polyVertsRadar)
 
 # --- Generate ROI from coordiantes (above) create 2D boolean array to mask with ---
-xp,yp = LISGridLon.flatten(),LISGridLat.flatten()
-points = np.vstack((xp,yp)).T
-path = Path(polyVerts)
-grid = path.contains_points(points)
-grid = grid.reshape(np.shape(LISGridLon))
-LISDataMasked = np.ma.masked_array(LISData, np.invert(grid))
+xpROI,ypROI = LISGridLon.flatten(),LISGridLat.flatten()
+pointsROI = np.vstack((xpROI,ypROI)).T
+pathROI = Path(polyVertsROI)
+gridROI = pathROI.contains_points(pointsROI)
+gridROI = gridROI.reshape(np.shape(LISGridLon))
+LISDataMasked = np.ma.masked_array(LISData, np.invert(gridROI))
+
+xpRadar,ypRadar = xlocs.flatten(),ylocs.flatten()
+pointsRadar = np.vstack((xpRadar,ypRadar)).T
+pathRadar = Path(polyVertsRadar)
+gridRadar = pathRadar.contains_points(pointsRadar)
+gridRadar = gridRadar.reshape(np.shape(xlocs))
+rDataMasked = np.ma.masked_array(rData, np.invert(gridRadar))
 
 # --- Clip our masked array, create sub-array of data and rotate ---
-i, j = np.where(grid)
-indices = np.meshgrid(np.arange(min(i), max(i) + 1), np.arange(min(j), max(j) + 1), indexing='ij')
-LISDataMaskedClip = LISData[indices]
-LISDataMaskClip = grid[indices]
+iROI, jROI = np.where(gridROI)
+indicesROI = np.meshgrid(np.arange(min(iROI), max(iROI) + 1), np.arange(min(jROI), max(jROI) + 1), indexing='ij')
+LISDataMaskedClip = LISData[indicesROI]
+LISDataMaskClip = gridROI[indicesROI]
 #print("dimsPreRot", LISDataMaskedClip.shape)
 #print("maskDimsPreRot", LISDataMaskClip.shape)
 LISDataMaskedClip = LISDataMaskedClip*LISDataMaskClip
 LISDataMaskedClip[LISDataMaskedClip > 100] = np.nan									# Replace our no data "9999" values with "nan"
-LISAligned = rotate(LISDataMaskedClip, (testLocBearing * 180 / np.pi)-(90), resize=True, order=0)		# rotate about center with nearest neighbor parameters, offest 0deg to 90 (top-down wind vector)
+LISAligned = rotate(LISDataMaskedClip, (testLocROIBearing * 180 / np.pi)-(90), resize=True, order=0)		# rotate about center with nearest neighbor parameters, offest 0deg to 90 (top-down wind vector)
 LISAligned[LISAligned == 0.0] = np.nan							# Replace out of ROI data w/ "nan"
 print('Alignment Checksum - Masked (unrotated) %9.0f Aligned (rotated) %9.0f' %(np.nansum(LISDataMaskedClip),np.nansum(LISAligned)))
 LISAligned = LISAligned[int((LISAligned.shape[0]-68)/2):int(((LISAligned.shape[0]-68)/2)+68)\
 						,int((LISAligned.shape[1]-68)/2):int(((LISAligned.shape[1]-68)/2)+68)]
+
+iRadar, jRadar = np.where(gridRadar)
+indicesRadar = np.meshgrid(np.arange(min(iRadar), max(iRadar) + 1), np.arange(min(jRadar), max(jRadar) + 1), indexing='ij')
+rDataMaskedClip = rData[indicesRadar]
+rDataMaskClip = gridRadar[indicesRadar]
+#print("dimsPreTM", rDataMaskedClip.shape)
+#print("maskDimsPreTM", rDataMaskClip.shape)
+rDataMaskedClip = rDataMaskedClip*rDataMaskClip
+#print("dimsPostTM", rDataMaskedClip.shape)
 
 # --- Create basic stats ---
 LISAlignedMeanDep = LISAligned - np.nanmean(LISAligned)			# calculate our mean departures
@@ -215,7 +251,6 @@ LISGradient = np.gradient(LISAligned, axis=1)
 
 # --- Plot data and create output ---
 vmin, vmax = 5, 25
-vadj = .101
 cmap = plt.cm.twilight_shifted_r
 cmap2 = plt.cm.bone_r
 normRawData = colors.SymLogNorm(linthresh=0.1, linscale=1, vmin=vmin, vmax=vmax)
@@ -226,10 +261,10 @@ fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(14, 12), constrained_layout=
                          gridspec_kw={'width_ratios': [10, 1, 10, 1], 'height_ratios': [10, 1, 10, 1], 'wspace': 0.0, 'hspace': 0.0})
 
 #calculate the crds of our ROI for projecting the basemap
-llcrnrlon= np.min(polyVerts[:,0] -.125)
-llcrnrlat= np.min(polyVerts[:,1] -.125)
-urcrnrlon= np.max(polyVerts[:,0] +.125)
-urcrnrlat= np.max(polyVerts[:,1] +.125)
+llcrnrlon= np.min(polyVertsROI[:,0] -180)
+llcrnrlat= np.min(polyVertsROI[:,1] -90)
+urcrnrlon= np.max(polyVertsROI[:,0] +180)
+urcrnrlat= np.max(polyVertsROI[:,1] +40)
 
 m = Basemap( projection='lcc', resolution='c', rsphere=(6378137.00,6356752.3142), 
 			llcrnrlon=llcrnrlon, urcrnrlon=urcrnrlon, llcrnrlat=llcrnrlat, 
@@ -245,10 +280,15 @@ m = Basemap( projection='lcc', resolution='c', rsphere=(6378137.00,6356752.3142)
 LISPlotX, LISPlotY = m(LISGridLon, LISGridLat)
 m.drawparallels(np.arange(-90.,120.,1),labels=[1,0,0,0])
 m.drawmeridians(np.arange(-120.,-80.,1),labels=[0,0,0,1])
-sp00 = m.pcolormesh(LISPlotX,LISPlotY,LISDataMasked,shading='flat', cmap=cmap, norm=normRawData)
+#sp00 = m.pcolormesh(LISPlotX,LISPlotY,LISDataMasked,shading='flat', cmap=cmap, norm=normRawData)
 UPlotX, UPlotY = m(UGridLon, UGridLat)
-axes[0][0].barbs(UPlotX, UPlotY, U, V, pivot='tip', barbcolor='#333333')	
-plt.colorbar(sp00, orientation='vertical', shrink=0.5, ax=axes[0][1])
+#axes[0][0].barbs(UPlotX, UPlotY, U, V, pivot='tip', barbcolor='#333333')	
+#plt.colorbar(sp00, orientation='vertical', shrink=0.5, ax=axes[0][1])
+
+normRadar, cmapRadar = colortables.get_with_steps("NWSReflectivity", 16, 16)
+print("non" , xlocs[indicesRadar])
+print("with", xlocs[indicesRadar]+float(args["lat_lon"][1]))
+sp03_rad = m.pcolormesh(xlocs[indicesRadar]+gridTestLoc[1], ylocs[indicesRadar]+gridTestLoc[0], rDataMaskedClip, norm=normRadar, cmap=cmapRadar)
 
 #plot the distribution of RSM values
 #axes[1][0].hist(LISAligned.flatten(), bins='fd')
@@ -263,7 +303,6 @@ axes[0][2].axhline(y=0, color='k')
 axes[0][2].axvline(x=0, color='k')
 plt.colorbar(sp02, orientation='vertical', shrink=0.5, ax=axes[0][3])
 
-sp03 = axes[0][3].imshow(z1, cmap=cmap, extent=extent, aspect=10/1, origin='lower', norm=normRawData)
 axes[0][3].set_xticks([])
 axes[0][3].yaxis.tick_right()
 
