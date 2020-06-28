34577a4f6a9fd0250a9c048fa8d541a915c154da
skye.skyehawk@gmail.com <dnaKgGyuWKi4>
2020-06-28 15:36:57 -0400

    - More robust Slope acquisition for time series

--- NEXRADL3_Extract_multi_2.py
+++ NEXRADL3_Extract_multi_2.py
@@ -110,6 +110,7 @@ def main():
 	print(resultsDF[['areaValue','refValue']].head(5))
 
 	# --- Plot time series---
+	print('Plotting Slices...')
 	fig, axes = plt.subplots(8, 8, figsize=(30, 30))
 	date_format = mpl_dates.DateFormatter('%H:%Mz')
 
@@ -144,12 +145,12 @@ def main():
 
 	# pull data out of DF to make code cleaner
 	datetimes = resultsDF['datetime'].tolist()
-	#elapsedtimes = list(map(lambda x: x - min(datetimes), datetimes))		# not currently used, need to get this working
-	areaValues = resultsDF['areaValue'].tolist()							# area ≥ 35dbz within ROI
-	refValues = (np.array(resultsDF['refValue'].tolist())-65) * 0.5			# mean reflectivity ≥ 35dbz within ROI (conversion: (val-65)*0.5) [https://mesonet.agron.iastate.edu/GIS/rasters.php?rid=2]
-	#areaRefValues = np.multiply(areaValues, refValues)						# product of area and reflectivity
-	varValues = resultsDF['varRefValue'].tolist()							# variance of mean reflectivity ≥ 35dbz within ROI
-	cvValues = np.array([a / b for a, b in zip(varValues, refValues)])*0.5	# coeff. of variation for mean reflectivity ≥ 35dbz within ROI
+	#elapsedtimes = list(map(lambda x: x - min(datetimes), datetimes))						# not currently used, need to get this working
+	areaValues = resultsDF['areaValue'].tolist()											# area ≥ 35dbz within ROI
+	refValues = (np.array(resultsDF['refValue'].tolist())-65) * 0.5							# mean reflectivity ≥ 35dbz within ROI (conversion: (val-65)*0.5) [https://mesonet.agron.iastate.edu/GIS/rasters.php?rid=2]
+	#areaRefValues = np.multiply(areaValues, refValues)										# product of area and reflectivity
+	varValues = resultsDF['varRefValue'].tolist()											# variance of mean reflectivity ≥ 35dbz within ROI
+	cvValues = np.array([a / b for a, b in zip(varValues, refValues)])*0.5					# coeff. of variation for mean reflectivity ≥ 35dbz within ROI
 
 	# Frequency
 	N = len(refValues)
@@ -165,55 +166,94 @@ def main():
 	areaCVValuesNormalized = np.multiply(areaNorm, cvNorm)
 
 	# Curve Smoothing
-	window = len(resultsDF.index)//8 							#approx 2 hours/ 8 ~15 mins ----> number of samples in moving average ( helps counteract more visible noise in higher temporal resolution data)
-	yAreaAvg = movingaverage(areaValues, window)				# create moving averages for time series'
-	yRefAvg = movingaverage(refValues, window)
-	yCVAvg = movingaverage(cvValues, window)
-	yAreaCVNormAvg = movingaverage(areaCVValuesNormalized, window)
+	window = len(resultsDF.index)//8 														# ~2 hours/8 = ~15 mins ----> number of samples in moving average ( helps counteract more visible noise in higher temporal resolution data)
+	yAreaAvg = movingaverage(areaValues, window)[window//2:-window//2]						# create moving averages for time series'
+	yRefAvg = movingaverage(refValues, window)[window//2:-window//2]	
+	yCVAvg = movingaverage(cvValues, window)[window//2:-window//2]	
+	yAreaCVNormAvg = movingaverage(areaCVValuesNormalized, window)[window//2:-window//2]	
 
-	# local maxima on smoothed curves
-	areaMax = argrelmax(yAreaAvg)
-	areaMin = argrelmin(yAreaAvg)
-	refMax = argrelmax(yRefAvg)
-	refMin = argrelmin(yRefAvg)
-	cvMax = argrelmax(yCVAvg)
-	cvMin = argrelmin(yCVAvg)
-	yAreaCVNormMax = argrelmax(yAreaCVNormAvg)
-	yAreaCVNormMin = argrelmin(yAreaCVNormAvg)
+	# local minima & maxima on smoothed curves
+	minTemporalwindow = window*2
 
-	# Slopes of Build-up Lines
+	areaLocalMax = argrelmax(yAreaAvg)
+	areaLocalMin = argrelmin(yAreaAvg)
+	endpoints = []
+	if yAreaAvg[0] <= np.all(yAreaAvg[1:window]) or yAreaAvg[0] >= np.all(yAreaAvg[1:window]):
+		endpoints.append(0)
+	if yAreaAvg[-1] <= np.all(yAreaAvg[len(yAreaAvg-1)-window:-2]) or yAreaAvg[-1] >= np.all(yAreaAvg[len(yAreaAvg-1)-window:-2]):
+		endpoints.append(len(yAreaAvg)-1) 
+	areaExtremaRaw = sorted(areaLocalMax[0].tolist()+areaLocalMin[0].tolist()+endpoints)	# combine mins, maxes, and endpoints (if endpoints are an extreme) then sort
+	areaExtrema = [x for x in areaExtremaRaw[1:] if x-areaExtremaRaw[0]>=minTemporalwindow] # remove maxima that are within threshold of first one
+	areaExtrema = [areaExtremaRaw[0]]+areaExtrema							# add back in forst one to begining
+	print(f'Area Extrema: {areaExtrema}')
+
+	refLocalMax = argrelmax(yRefAvg)
+	refLocalMin = argrelmin(yRefAvg)
+	endpoints = []
+	if yRefAvg[0] <= np.all(yRefAvg[1:window]) or yRefAvg[0] >= np.all(yRefAvg[1:window]):
+		endpoints.append(0)
+	if yRefAvg[-1] <= np.all(yRefAvg[len(yRefAvg-1)-window:-2]) or yRefAvg[-1] >= np.all(yRefAvg[len(yRefAvg-1)-window:-2]):
+		endpoints.append(len(yRefAvg)-1) 
+	refExtremaRaw = sorted(refLocalMax[0].tolist()+refLocalMin[0].tolist()+endpoints)
+	refExtrema = [x for x in refExtremaRaw[1:] if x-refExtremaRaw[0]>=minTemporalwindow]
+	refExtrema = [refExtremaRaw[0]]+refExtrema
+	print(f'Ref Extrema: {refExtrema}')
+	
+	#cvLocalMax = argrelmax(yCVAvg)
+	#cvLocalMin = argrelmin(yCVAvg)
+	#endpoints = []
+	#if yCVAvg[0] <= np.all(yCVAvg[1:window]) or yCVAvg[0] >= np.all(yCVAvg[1:window]):
+	#	endpoints.append(0)
+	#if yCVAvg[-1] <= np.all(yCVAvg[len(yCVAvg-1)-window:-2]) or yCVAvg[-1] >= np.all(yCVAvg[len(yCVAvg-1)-window:-2]):
+	#	endpoints.append(len(yCVAvg)-1) 
+	#cvExtremaRaw = sorted(cvLocalMax[0].tolist()+cvLocalMin[0].tolist()+endpoints)
+	#cvExtrema = [x for x in cvExtremaRaw[1:] if x-cvExtremaRaw[0]>=minTemporalwindow]
+	#cvExtrema = [cvExtremaRaw[0]]+cvExtrema
+	#print(f'CV Extrema: {cvExtrema}')
+
+	yAreaCVNormLocalMax = argrelmax(yAreaCVNormAvg)
+	yAreaCVNormLocalMin = argrelmin(yAreaCVNormAvg)
+	endpoints = []
+	if yAreaCVNormAvg[0] <= np.all(yAreaCVNormAvg[1:window]) or yAreaCVNormAvg[0] >= np.all(yAreaCVNormAvg[1:window]):
+		endpoints.append(0)
+	if yAreaCVNormAvg[-1] <= np.all(yAreaCVNormAvg[len(yAreaCVNormAvg-1)-window:-2]) or yAreaCVNormAvg[-1] >= np.all(yAreaCVNormAvg[len(yAreaCVNormAvg-1)-window:-2]):
+		endpoints.append(len(yAreaCVNormAvg)-1) 
+	yAreaCVNormExtremaRaw = sorted(yAreaCVNormLocalMax[0].tolist()+yAreaCVNormLocalMin[0].tolist()+endpoints)
+	yAreaCVNormExtrema = [x for x in yAreaCVNormExtremaRaw[1:] if x-yAreaCVNormExtremaRaw[0]>=minTemporalwindow]
+	yAreaCVNormExtrema = [yAreaCVNormExtremaRaw[0]]+yAreaCVNormExtrema
+	print(f'AreaCVNorm Extrema: {yAreaCVNormExtrema}')
+	
+
+	# Find slopes of Build-up Lines
 	# 	Area
-	xArea = np.array(datetimes)[np.array([1,areaMax[0][0]])]
-	xAreaDiff = xArea[1] - xArea[0]
-	yArea = yAreaAvg[np.array([1,areaMax[0][0]])]
-	yAreaDiff = yArea[1] - yArea[0]
-	slopeArea = np.arctan(yAreaDiff/xAreaDiff.seconds)
+	xArea = np.array(datetimes[window//2:-window//2])[np.array([areaExtrema[0],areaExtrema[1]])]		# grab datetime (x component) of the leftmost bounds (determined by window size), and the first extreme on the smoothed curve (sm curve is already bound by window, we need to apply bounds to datetimes)
+	xAreaDiff = xArea[1] - xArea[0]															# subtract the later value from the former to get our delta x
+	yArea = yAreaAvg[np.array([areaExtrema[0],areaExtrema[1]])]								# grab the values (y component) of the sm curve at the begining and at the first extreme
+	yAreaDiff = yArea[1] - yArea[0]															# subtract to find delta y
+	slopeArea = np.arctan(yAreaDiff/xAreaDiff.seconds)										# calc the slope angle
 	print (f'Slope Area: {slopeArea}')
 
 	#   Reflectivity
-	xRef = np.array(datetimes)[np.array([1,areaMax[0][0]])]
+	xRef = np.array(datetimes[window//2:-window//2])[np.array([refExtrema[0],refExtrema[1]])]
 	xRefDiff = xRef[1] - xRef[0]
-	yRef = yRefAvg[np.array([1,refMax[0][0]])]
+	yRef = yRefAvg[np.array([refExtrema[0], refExtrema[1]])]
 	yRefDiff = yRef[1] - yRef[0]
 	slopeRef = np.arctan(yRefDiff/xRefDiff.seconds)
 	print (f'Slope Reflectivity: {slopeRef}')
 
 	# 	Product of Area and CV of ref
-	xProduct = np.array(datetimes)[np.array([1,yAreaCVNormMax[0][0]])]
+	xProduct = np.array(datetimes[window//2:-window//2])[np.array([yAreaCVNormExtrema[0],yAreaCVNormExtrema[1]])]
 	XProductDiff = xProduct[1] - xProduct[0]
-	yProduct = yAreaCVNormAvg[np.array([1,yAreaCVNormMax[0][0]])]
+	yProduct = yAreaCVNormAvg[np.array([yAreaCVNormExtrema[0],yAreaCVNormExtrema[1]])]
 	yProductDiff = yProduct[1] - yProduct[0]
 	slopeProduct = np.arctan(yProductDiff/XProductDiff.seconds)
 	print (f'Slope Product: {slopeProduct}')
 
-	#print(f'Area Mins: {yAreaAvg[areaMin]}; Area Maxs: {yAreaAvg[areaMax]}')
-	#print(f'CV Mins: {yCVAvg[cvMin]}; CV Maxs: {yCVAvg[cvMax]}')
-
 	print('Plotting Additional Data and Saving Output...')
 	# Area for Reflectivity ≥ 35dbz
 	axes[-1][-5].plot_date(datetimes,areaValues,linestyle='solid', ms=2)
-	axes[-1][-5].plot_date(datetimes[window//2:-window//2], yAreaAvg[window//2:-window//2], linestyle='solid', ms=2)
-	axes[-1][-5].plot_date(np.array(datetimes)[np.array([1,areaMax[0][0]])], yAreaAvg[np.array([1,areaMax[0][0]])], linestyle="solid", ms=2)
+	axes[-1][-5].plot_date(datetimes[window//2:-window//2], yAreaAvg, linestyle='solid', ms=2)
+	axes[-1][-5].plot_date(np.array(datetimes[window//2:-window//2])[np.array([areaExtrema[0],areaExtrema[1]])], yAreaAvg[np.array([areaExtrema[0],areaExtrema[1]])], linestyle="solid", ms=2)
 	axes[-1][-5].legend(['Area Delta','Sm. Area Delta', 'Build-up Rate'])
 	axes[-1][-5].xaxis.set_major_formatter(date_format)
 	plt.setp(axes[-1][-5].xaxis.get_majorticklabels(), rotation=45, ha="right", rotation_mode="anchor" )
@@ -222,8 +262,10 @@ def main():
 	# TODO: map y axis to dbz for output
 	# Mean of Reflectivity ≥ 35dbz
 	axes[-1][-4].plot_date(datetimes,refValues,linestyle='solid', ms=2)
-	axes[-1][-4].plot_date(datetimes[window//2:-window//2], yRefAvg[window//2:-window//2], linestyle='solid', ms=2)
-	axes[-1][-4].plot_date(np.array(datetimes)[np.array([1,refMax[0][0]])], yRefAvg[np.array([1,refMax[0][0]])], linestyle="solid", ms=2)
+	#axes[-1][-4].plot_date(datetimes[window//2:-window//2], yRefAvg, linestyle='solid', ms=2)
+	#axes[-1][-4].plot_date(np.array(datetimes[window//2:-window//2])[np.array([0,refLocalMax[0][0]])], yRefAvg[np.array([0,refLocalMax[0][0]])], linestyle="solid", ms=2)
+	axes[-1][-4].plot_date(datetimes[window//2:-window//2], yRefAvg, linestyle='solid', ms=2)
+	axes[-1][-4].plot_date(np.array(datetimes[window//2:-window//2])[np.array([refExtrema[0],refExtrema[1]])], yRefAvg[np.array([refExtrema[0],refExtrema[1]])], linestyle="solid", ms=2)
 	axes[-1][-4].legend(['Ref Delta','Sm. Ref Delta', 'Build-up Rate'])
 	axes[-1][-4].xaxis.set_major_formatter(date_format)
 	plt.setp(axes[-1][-4].xaxis.get_majorticklabels(), rotation=45, ha="right", rotation_mode="anchor" )
@@ -231,8 +273,8 @@ def main():
 	
 	# Product of cv reflectivity and area
 	axes[-1][-3].plot_date(datetimes,areaCVValuesNormalized,linestyle='solid', ms=2)
-	axes[-1][-3].plot_date(datetimes[window//2:-window//2], yAreaCVNormAvg[window//2:-window//2], linestyle='solid', ms=2)
-	axes[-1][-3].plot_date(np.array(datetimes)[np.array([1,yAreaCVNormMax[0][0]])], yAreaCVNormAvg[np.array([1,yAreaCVNormMax[0][0]])], linestyle="solid", ms=2)
+	axes[-1][-3].plot_date(datetimes[window//2:-window//2], yAreaCVNormAvg, linestyle='solid', ms=2)
+	axes[-1][-3].plot_date(np.array(datetimes[window//2:-window//2])[np.array([yAreaCVNormExtrema[0],yAreaCVNormExtrema[1]])], yAreaCVNormAvg[np.array([yAreaCVNormExtrema[0],yAreaCVNormExtrema[1]])], linestyle="solid", ms=2)
 	axes[-1][-3].legend(['Area*cv_Ref Delta','Sm. Area*cv_Ref Delta', 'Build-up Rate'])
 	axes[-1][-3].xaxis.set_major_formatter(date_format)
 	plt.setp(axes[-1][-3].xaxis.get_majorticklabels(), rotation=45, ha="right", rotation_mode="anchor" )
@@ -240,7 +282,7 @@ def main():
 
 	# Coeff. of Variance of Reflectivity ≥ 35dbz
 	axes[-1][-2].plot_date(datetimes,cvValues,linestyle='solid', ms=2)
-	axes[-1][-2].plot_date(datetimes[window//2:-window//2], yCVAvg[window//2:-window//2], linestyle='solid', ms=2)
+	axes[-1][-2].plot_date(datetimes[window//2:-window//2], yCVAvg, linestyle='solid', ms=2)
 	axes[-1][-2].legend(['CV Delta','Sm. CV Delta'])
 	axes[-1][-2].xaxis.set_major_formatter(date_format)
 	plt.setp(axes[-1][-2].xaxis.get_majorticklabels(), rotation=45, ha="right", rotation_mode="anchor" )
@@ -251,7 +293,7 @@ def main():
 	axes[-1][-1].semilogy(xf[1:N//2], 2.0/N * np.abs(ywf[1:N//2]), '-r')
 	axes[-1][-1].legend(['FFT','FFT w. Window'])
 	#axes[-1][-1].plot(xf, 2.0/N * np.abs(yf[0:N//2]),linestyle='solid', ms=2)
-	#axes[-1][-1].plot_date(datetimes[window//2:-window//2], yCVAvg[window//2:-window//2], linestyle='solid')
+	#axes[-1][-1].plot_date(datetimes, yCVAvg, linestyle='solid')
 	#axes[-1][-1].xaxis.set_major_formatter(date_format)
 	plt.setp(axes[-1][-1].xaxis.get_majorticklabels(), rotation=45, ha="right", rotation_mode="anchor" )
 	axes[-1][-1].set_title('Testing Plot (Frequency)')
