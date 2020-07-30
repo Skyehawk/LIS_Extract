ccaa019b2a4f5d1a8b56b9fe07b2067f0b591b67
skye.skyehawk@gmail.com <dnaKgGyuWKi4>
2020-07-30 13:42:19 -0400

    Support for NEXRAD L2 Files direct from AWS S3 buckets via boto

---
+++ RadarROI_L2.py
@@ -0,0 +1,165 @@
+# NEXRAD Data Encapsulation - v0.1.0
+# Python Version: 3.7.3
+#
+# @author: Skye Leake
+# @date: 2020-04-28
+#
+# Updated
+# 2020-04-28
+#
+
+# --- Imports ---
+import numpy as np
+from matplotlib.path import Path
+from Transformation_Matrix_2 import comp_matrix
+from RadarSlice_L2 import RadarSlice_L2
+
+class RadarROI_L2(RadarSlice_L2):
+    
+    @property
+    def clippedData(self):
+        if not hasattr(self, '_clippedData'):
+            self._clippedData = np.empty((1))
+        return self._clippedData
+
+    @property
+    def clippedRangeMap(self):
+        if not hasattr(self, '_clippedRangeMap'):
+            self._clippedRangeMap = np.empty((1))
+        return self._clippedRangeMap
+
+    @property
+    def mask(self):
+        return self._mask
+    
+    @property
+    def area(self):
+        if not hasattr(self, '_area'):
+            self._area = -1.0
+        return self._area
+
+    @property
+    def meanReflectivity(self):
+        if not hasattr(self, '_meanReflectivity'):
+            self._meanReflectivity = -1.0
+        return self._meanReflectivity
+
+    @property
+    def varReflectivity(self):
+        if not hasattr(self, '_varReflectivity'):
+            self._varReflectivity = -1.0
+        return self._varReflectivity
+
+    @property
+    def polyVerts(self):
+        if not hasattr(self, '_polyVerts'):
+            self._polyVerts = []
+        return self._polyVerts
+    
+    @property
+    def tm(self):
+        if not hasattr(self,'_tm'):
+            self._tm = comp_matrix(scale=np.ones(2), rotation=np.zeros(2), 
+                                    shear=np.ones(2), translation=np.zeros(2))
+        return self._tm
+
+    @clippedData.setter
+    def clippedData(self, value):
+        self._clippedData = value
+
+    @clippedRangeMap.setter
+    def clippedRangeMap(self, value):
+        self._clippedRangeMap = value
+
+    @mask.setter
+    def mask(self, value):
+        self._mask = value
+
+    @area.setter
+    def area(self,value):
+        self._area = value
+
+    @meanReflectivity.setter
+    def meanReflectivity(self, value):
+        self._meanReflectivity = value
+
+    @varReflectivity.setter
+    def varReflectivity(self, value):
+        self._varReflectivity = value
+
+    @polyVerts.setter
+    def polyVerts(self,value):
+        self._polyVerts = value
+
+    @tm.setter
+    def tm(self, value):
+        self._tm = value
+
+    #Override
+    def __init__(self, radarFile, sweep=0):
+        super(RadarROI_L2, self).__init__(radarFile, sweep)
+
+    def extractROI(self, baseCrds=None, baseBearing=0.0, scaleFactor=1.0):
+        if baseCrds is None:
+            baseCrds = np.array([(1.5,1.0,0.0,1.0),
+                        (1.5,-1.0,0.0,1.0),
+                        (-0.5,-1.0,0.0,1.0),
+                        (-0.5,1.0,0.0,1.0),
+                        (1.5,1.0,0.0,1.0)])    #default crds of bounding box (Gridded degrees)
+        
+        self.tm = comp_matrix(scale=np.ones(3)*scaleFactor, rotation=np.array([0,0, baseBearing]), 
+                        shear=np.ones(3), translation=np.zeros(3))
+
+        self.polyVerts = self.tm.dot(baseCrds.T).T[:,:2]    # Apply transformation Matrix, remove padding, and re-transpose
+        
+        # --- Generate ROI from coordiantes (above) create 2D boolean array to mask with ---
+        xp,yp = self.xlocs.flatten(),self.ylocs.flatten()
+        points = np.vstack((xp,yp)).T
+        path = Path(self.polyVerts)
+        grid = path.contains_points(points)
+        grid = grid.reshape(np.shape(self.xlocs))
+        rDataMasked = np.ma.masked_array(self.data, np.invert(grid))
+
+        # --- Clip our masked array, create sub-array of data and rotate ---
+        i, j = np.where(grid)
+        self.mask = np.meshgrid(np.arange(min(i), max(i) + 1), np.arange(min(j), max(j) + 1), indexing='ij')
+        rDataMaskedClip = self.data[self.mask]
+        rDataMaskClip = grid[self.mask]
+        self.clippedData = rDataMaskedClip*rDataMaskClip
+
+        rRangeMapMaskedClip = self.rangeMap[:,:-1][self.mask]
+        self.clippedRangeMap = rRangeMapMaskedClip*rDataMaskClip
+
+        self.xlocs = self.xlocs[self.mask]
+        self.ylocs = self.ylocs[self.mask]
+        return self.clippedData
+
+    #Override
+    def find_area(self, reflectThresh=0.0):
+        self.stackedData = np.dstack([self.clippedData, self.clippedRangeMap])                     # remove last range gate on the rangeMap
+        #dep    #rsStackedData = self.stackedData.reshape((self.stackedData.shape[0])*(self.stackedData.shape[1]),2)  #Sholdn't need to rehape here, but we don't need the extra dim and would need to collapse it later
+        #dep    #self.area = sum(map(lambda i: i >= reflectThresh, self.data.flatten()))
+        #dep    #self.area = sum((rsStackedData * np.array(list(map(lambda i: i >= reflectThresh,rsStackedData)))).T[1]) # Grab the underlying cell area values
+        self.area = np.nansum(np.where(self.stackedData[:,:,0]>= reflectThresh, self.stackedData[:,:,1], np.nan))
+        return self.area
+
+    #Override
+    def find_mean_reflectivity(self, reflectThresh=0.0):
+        if self.area == -1.0:
+            find_area(reflectThresh)
+        self.stackedData = np.dstack([self.clippedData, self.clippedRangeMap])
+        self.meanReflectivity = np.nansum(np.where(self.stackedData[:,:,0]>= reflectThresh, self.stackedData[:,:,0]*self.stackedData[:,:,1], np.nan))/self.area #return product of reflectivity & weighting factor where >= thresh
+        return self.meanReflectivity
+
+    #Override
+    def find_variance_reflectivity(self, reflectThresh=0.0):
+        if self.area == -1.0:
+            find_area(reflectThresh)
+        self.stackedData = np.dstack([self.clippedData, self.clippedRangeMap])
+        self.varReflectivity = np.nanvar(np.where(self.stackedData[:,:,0]>= reflectThresh, self.stackedData[:,:,0]*self.stackedData[:,:,1], np.nan))/self.area #return product of reflectivity & weighting factor where >= thresh
+        #self.varReflectivity = np.var(np.array(list(filter(lambda x: x >= reflectThresh, self.clippedData.flatten()))))
+        return self.varReflectivity
+
+    #Override
+    def __str__(self):
+        return ('string method for radarRoi')
---
+++ RadarSlice_L2.py
@@ -0,0 +1,222 @@
+# NEXRAD Data Encapsulation - v0.1.0
+# Python Version: 3.7.3
+#
+# @author: Skye Leake
+# @date: 2020-04-27
+#
+# Updated
+# 2020-04-28
+#
+
+# --- Imports ---
+import warnings
+import operator
+
+import numpy as np
+#from datetime import datetime, timedelta
+import datetime
+from metpy.io import Level2File
+
+from Transformation_Matrix_2 import comp_matrix
+
+class RadarSlice_L2(object):
+
+    #Full list of properties avaliable at: https://unidata.github.io/MetPy/latest/api/generated/metpy.io.Level3File.html
+
+    @property
+    def metadata(self):
+        if not hasattr(self,'_metadata'):
+            self._metadata = None
+            warnings.warn("Radar_Slice: No input metadata parsed on __init__", UserWarning)
+        return self._metadata
+    
+    @property
+    def sweepDateTime(self):
+        if not hasattr(self,'_sweepDateTime'):
+            self._sweepDateTime = None
+            warnings.warn("Radar_Slice: No input datetime parsed on __init__ , check input metadata",  UserWarning)
+        return self._sweepDateTime
+
+    @property
+    def data(self):
+        if not hasattr(self,'_data'):
+            self._data = None
+            warnings.warn("Radar_Slice: No input data parsed on __init__", UserWarning)
+        return self._data
+
+    @property
+    def rangeMap(self):
+        if not hasattr(self,'_rangeMap'):
+            self._rangeMap = None
+        return self._rangeMap
+
+    @property
+    def reflectivityUnits(self):
+        if not hasattr(self, '_reflectivityUnits'):
+            self._reflectivityUnits = '8Bit'
+        return self._reflectivityUnits
+    
+    @property
+    def sensorData(self):
+        if not hasattr(self,'_sensorData'):
+            self._sensorData = None
+            warnings.warn("Radar_Slice: No input sensor metadata parsed on __init__", UserWarning)
+        return self._sensorData
+
+    @property
+    def mapData(self):
+        if not hasattr(self,'_mapData'):
+            self._mapData = None
+            warnings.warn("Radar_Slice: No input sensor data mapping values parsed on __init__", UserWarning)
+        return self._mapData
+    
+    @property
+    def xlocs(self):
+        return self._xlocs
+
+    @property
+    def ylocs(self):
+        return self._ylocs
+
+    @property
+    def area(self):
+        if not hasattr(self, '_area'):
+            self._area = -1.0
+        return self._area
+
+    @property
+    def meanReflectivity(self):
+        if not hasattr(self, '_meanReflectivity'):
+            self._meanReflectivity = -1.0
+        return self._meanReflectivity
+
+    @property
+    def varReflectivity(self):
+        if not hasattr(self, '_varReflectivity'):
+            self._varReflectivity = -1.0
+        return self._varReflectivity
+
+    @metadata.setter
+    def metadata(self, value):
+        self._metadata = value
+
+    @sweepDateTime.setter
+    def sweepDateTime(self, value):
+        self._sweepDateTime = value
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
+    @rangeMap.setter
+    def rangeMap(self, value):
+        self._rangeMap = value
+
+    @reflectivityUnits.setter
+    def reflectivityUnits(self, value):
+        self._reflectivityUnits = value
+
+    @sensorData.setter
+    def sensorData(self, value):
+        self._sensorData = value
+
+    @mapData.setter
+    def mapData(self, value):
+        self._mapData = value
+
+    @xlocs.setter
+    def xlocs(self, value):
+        self._xlocs = value
+
+    @ylocs.setter
+    def ylocs(self, value):
+        self._ylocs = value
+
+    @area.setter
+    def area(self,value):
+        self._area = value
+
+    @meanReflectivity.setter
+    def meanReflectivity(self,value):
+        self._meanReflectivity = value
+
+    @varReflectivity.setter
+    def varReflectivity(self, value):
+        self._varReflectivity = value
+
+    # Constructor (init)
+    def __init__(self, radarFile, sweep=0):
+        f = radarFile
+        self.az = np.array([ray[0].az_angle for ray in f.sweeps[sweep]])                    #create array of all azimuth angles within sweep
+        ref_hdr = f.sweeps[sweep][0][4][b'REF'][0]                                          #get header of reflectivity info
+        self.ref_range = np.arange(ref_hdr.num_gates) * ref_hdr.gate_width + ref_hdr.first_gate          #generate ranges of gates from the first gate outwards
+
+        self.ref = np.array([ray[4][b'REF'][1] for ray in f.sweeps[sweep]])
+
+        self.data = np.ma.array(self.ref)
+        self.data[np.isnan(self.data)] = np.ma.masked                                            # Mask out the zeros
+
+        sweepDate = datetime.datetime(1970,1,1,0,0) + datetime.timedelta(f.sweeps[sweep][0][0][2] - 1)
+        self.sweepDateTime =  sweepDate + datetime.timedelta(milliseconds=f.sweeps[sweep][0][0][1])
+        #print(f'SweepDT{self.sweepDateTime}')
+        self.sensorData = {'siteID' : str(f.sweeps[sweep][0][0][0])[2:-1]}
+
+        #range map - km^2 at each bin in range*azimuth
+        rangeStep = ref_hdr.gate_width
+        l = lambda x:(1.0/len(self.az))*((np.pi*(x+ref_hdr.gate_width)**2)-(np.pi*(x)**2))
+        self.rangeMap = np.ones(tuple(map(operator.add,self.data[:, :-1].shape,(0,1)))) * \
+                                np.array([[l(xi) for xi in self.ref_range]])
+
+    def calc_cartesian(self):
+        # these values will be the same for both the data and for the rangeMap values
+        self.kmPerDeg = 111.0
+        self.xlocs = (self.ref_range * np.sin(np.deg2rad(self.az[0:, np.newaxis]))/self.kmPerDeg) # Convert az,range to x,y, change to deg from km
+        self.ylocs = (self.ref_range * np.cos(np.deg2rad(self.az[0:, np.newaxis]))/self.kmPerDeg)
+        return True
+
+    def shift_cart_orgin(self, offset):
+        #offset must be in decimal degrees for (lat,lon)
+        self.xlocs += offset[1]
+        self.ylocs += offset[0]
+
+        if self.ylocs.any() > 90 or self.ylocs.any() < -90:
+            warnings.warn("Radar_Slice: offset (lat) produces values out of expected range [-90:90]",  UserWarning)
+        if self.xlocs.any() > 180 or self.xlocs.any() < -180:
+            warnings.warn("Radar_Slice: offset (lon) produces values out of expected range [-180:180]",  UserWarning)
+        return True
+
+    def mask_zeros(self):
+        self.data[self.data == 0] = np.ma.masked                                            # convert 0s to masked
+        return True
+
+    def convert_dbz(self):
+        if self.reflectivityUnits == '8Bit':
+            self.data = (self.data-65)*0.5
+            self.reflectivityUnits = 'dBZ'
+        return True
+
+    def find_area(self, reflectThresh=0.0):
+        self.stackedData = np.dstack([self.data, self.rangeMap])                     # remove last range gate on the rangeMap
+        #dep    #rsStackedData = self.stackedData.reshape((self.stackedData.shape[0])*(self.stackedData.shape[1]),2)  #Sholdn't need to rehape here, but we don't need the extra dim and would need to collapse it later
+        #dep    #self.area = sum(map(lambda i: i >= reflectThresh, self.data.flatten()))
+        #dep    #self.area = sum((rsStackedData * np.array(list(map(lambda i: i >= reflectThresh,rsStackedData)))).T[1]) # Grab the underlying cell area values
+        self.area = np.nansum(np.where(self.stackedData[:,:,0]>= reflectThresh, self.stackedData[:,:,1], np.nan))
+        return self.area
+
+    def find_mean_reflectivity(self, reflectThresh=0.0):
+        if self.area == -1.0:
+            find_area(reflectThresh)
+        self.stackedData = np.dstack([self.data, self.rangeMap])
+        self.meanReflectivity = np.nansum(np.where(self.stackedData[:,:,0]>= reflectThresh, self.stackedData[:,:,0]*self.stackedData[:,:,1], np.nan))/self.area #return product of reflectivity & weighting factor where >= thresh
+        return self.meanReflectivity
+
+    def find_variance_reflectivity(self, reflectThresh=0.0):
+        if self.area == -1.0:
+            find_area(reflectThresh)
+        self.stackedData = np.dstack([self.data, self.rangeMap])
+        self.varReflectivity = np.nanvar(np.where(self.stackedData[:,:,0]>= reflectThresh, self.stackedData[:,:,0]*self.stackedData[:,:,1], np.nan))/self.area #return product of reflectivity & weighting factor where >= thresh
+        #self.varReflectivity = np.var(np.array(list(filter(lambda x: x >= reflectThresh, self.clippedData.flatten()))))
+        return self.varReflectivity
+
+    def __str__(self):
+        return ('string method for radarSlice')
